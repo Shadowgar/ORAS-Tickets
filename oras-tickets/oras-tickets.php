a1a8e48a8a0f30f3706bd68ffefc817c20d0b4d4
ShadowGar <rocco.paul@gmail.com>
2026-06-08 01:53:36 -0400

    Bump version to 0.4.0 in plugin header and constant definition

--- oras-tickets/oras-tickets.php
+++ oras-tickets/oras-tickets.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: ORAS Tickets
  * Description: Internal Event Tickets add-on for ORAS (Phase 1 MVP).
- * Version: 0.3.7
+ * Version: 0.4.0
  * Author: ORAS
  * Requires at least: 6.0
  * Requires PHP: 8.0
@@ -20,7 +20,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('ORAS_TICKETS_VERSION', '0.3.7');
+define('ORAS_TICKETS_VERSION', '0.4.0');
 define('ORAS_TICKETS_FILE', __FILE__);
 define('ORAS_TICKETS_DIR', plugin_dir_path(__FILE__));
 define('ORAS_TICKETS_URL', plugin_dir_url(__FILE__));
