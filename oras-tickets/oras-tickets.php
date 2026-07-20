9a47cf65068b07585d56a496633e27d50679a7ea
ShadowGar <rocco.paul@gmail.com>
2026-07-20 03:23:52 -0400

    Bump version to 0.4.37

--- oras-tickets/oras-tickets.php
+++ oras-tickets/oras-tickets.php
@@ -23,7 +23,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('ORAS_TICKETS_VERSION', '0.4.33');
+define('ORAS_TICKETS_VERSION', '0.4.37');
 define('ORAS_TICKETS_FILE', __FILE__);
 define('ORAS_TICKETS_DIR', plugin_dir_path(__FILE__));
 define('ORAS_TICKETS_URL', plugin_dir_url(__FILE__));
