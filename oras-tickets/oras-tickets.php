bae9691ec89e87140c239d1f60bc473f968ad960
ShadowGar <rocco.paul@gmail.com>
2026-07-20 03:38:03 -0400

    Bump version to 0.4.38

--- oras-tickets/oras-tickets.php
+++ oras-tickets/oras-tickets.php
@@ -4,7 +4,7 @@
  * Plugin Name: ORAS Tickets
  * Plugin URI: https://github.com/Shadowgar/ORAS-Tickets
  * Description: Internal Event Tickets add-on for ORAS (Phase 1 MVP).
- * Version: 0.4.37
+ * Version: 0.4.38
  * Author: Paul Rocco
  * Requires at least: 6.0
  * Requires PHP: 8.0
